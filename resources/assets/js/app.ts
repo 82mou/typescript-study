555206fd67551377665c6a6bb9812a7555434bbe
k-hatsushi <hybrid.theory.882244@gmail.com>
2020-05-23 11:24:41 +0900

    htmlに表示させて、Netlifyでホスティングするところまで

--- resources/assets/js/app.ts
+++ resources/assets/js/app.ts
@@ -1,9 +1,13 @@
+declare var document: Document
+
 import axios from "axios";
 import { Qiita } from "./qiita";
 
 export async function main() {
   const res = await axios.get<Qiita[]>("https://qiita.com/api/v2/items");
-  console.log(res.data);
+  const $target = document.querySelector(".js-target")!;
+  
+  $target.insertAdjacentHTML("beforeend", res.data[0].rendered_body);
   return res;
 }
 
