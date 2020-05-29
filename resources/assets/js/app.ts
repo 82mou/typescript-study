47b4336cdfad27d63d458c7251f8a76e9f487ace
k-hatsushi <hybrid.theory.882244@gmail.com>
2020-05-29 16:44:58 +0900

    戻す

--- resources/assets/js/app.ts
+++ resources/assets/js/app.ts
@@ -4,7 +4,7 @@ import { Qiita } from "./qiita";
 export async function main() {
   const res = await axios.get<Qiita[]>("https://qiita.com/api/v2/items");
   const $target = document.querySelector(".js-target")!;
-  
+  console.log(res);
   $target.insertAdjacentHTML("beforeend", res.data[0].rendered_body);
   return res;
 }
